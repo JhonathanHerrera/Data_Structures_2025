58bb2fdd0deb286c7a9b8bf23117753c6ed2dc50
JhonathanHerrera <herrerajhonathan@ufl.edu>
2025-09-07 15:33:57 -0400

    new template

---
+++ Graphs/Union_Find/Template/union_find_template.py
@@ -0,0 +1,13 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x]) 
+        return self.parent[x]
+
+    def union(self, x, y):
+        rootX, rootY = self.find(x), self.find(y)
+        if rootX != rootY:
+            self.parent[rootY] = rootX 
